480d6c1f4a3808ebb4516654ab41971dd36a9918
Keerthana <nskj.keerthi@gmail.com>
2025-04-21 15:50:06 +0530

    Rock paper scissor

---
+++ session2/number.py
@@ -0,0 +1,15 @@
+item1 = "milk"
+item2 = "apples"
+item3 = "bananas"
+
+qty1 = 2
+qty2 = 5
+qty3 = 12
+
+price1 = 65.00
+price2 = 95.50
+price3 = 40.50
+
+print(qty1, "packet", item1, "@ Rs.", price1)
+print(qty2,  item2, "@ Rs. ", price2)
+print(qty3, item3, "@rs.", price3)
